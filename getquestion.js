4e4fcd1a23b9bbbd1fbf7739af14465ac4ce6e6d
ABIMBOLA OLUDIPE <ao.oludipe@gmail.com>
2019-02-28 15:05:38 +0100

    delete function added

--- getquestion.js
+++ getquestion.js
@@ -4,15 +4,14 @@ $(document).ready(function(){
         type: "GET",
         url: 'http://localhost:3000/questions',
         success: function(data) {
+            var count = 0
             $.each(data, function(i, item){
-                question.append('<p><strong> ' + item.id + ')  ' + item.question + '</strong></p><p><input type = "checkbox"> A   ' + item.A +'</p><p><input type = "checkbox"> B    ' + item.B +'</p><p><input type = "checkbox"> C    '+ item.C +'</p>')
+                count++
+                question.append('<p><strong> ' + count + ')  ' + item.question + '</strong></p><p><input type = "checkbox"> A   ' + item.A +'</p><p><input type = "checkbox"> B    ' + item.B +'</p><p><input type = "checkbox"> C    '+ item.C +'</p>')
             })
         },
         error: function(){
             alert('error loading orders')
         }
     })
-
-
-
 })
