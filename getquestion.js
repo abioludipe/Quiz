0ce93c3e24389135255dacfb59fe9fd0f668aae9
ABIMBOLA OLUDIPE <ao.oludipe@gmail.com>
2019-03-01 09:56:15 +0100

    Take quiz function almost completed

--- getquestion.js
+++ getquestion.js
@@ -7,30 +7,39 @@ $(document).ready(function(){
             var count = 0
             $.each(data, function(i, item){
                 count++
-                question.append('<p><strong> ' + count + ')  ' + item.question + '</strong></p><p><input class = "options" type = "radio" name='+ item.id +'value = '+ item.A +'> A   ' + item.A +'</p><p><input class = "options" type = "radio" name= '+ item.id +'value = '+ item.B + '> B    ' + item.B +'</p><p><input class = "options" type = "radio" name='+ item.id +'value = '+ item.C + '> C    '+ item.C +'</p>')
+                question.append('<p><strong> ' + count + ')  ' + item.question + '</strong></p><p><input class = "options" type = "radio" name= '+ item.id +' value = '+ item.A + '> A   ' + item.A +'</p><p><input class = "options" type = "radio" name= '+ item.id +' value = '+ item.B + '> B    ' + item.B +'</p><p><input class = "options" type = "radio" name='+ item.id +' value = '+ item.C + '> C    '+ item.C +'</p><p><input class = "correct" type = radio name= ' + item.id +' value = '+ item.answer + '></p><p class = "final"></p>')
             })
         },
         error: function(){
             alert('error loading orders')
         }
+        
     })
 
     $("#finishQuestion").on("click", function(e){
         e.preventDefault()
-
             var guestOptions = $(".options")
-            console.log(guestOptions)
-            return
+            var corretAnswer = $(".correct")
             var guestAnswer = []
+            var correctArray = []
             for (var i = 0; i< guestOptions.length; i++) {
-                if(guestOptions[i].checked) {
-                   // guestAnswer.push(guestOptions[i])
-                   
-                    console.log(guestOptions[i])
-                    return
-                }
+                if (guestOptions[i].checked) {
+                    guestAnswer.push(guestOptions[i].value)
+                } 
             }
-            
-            
+            for (var i = 0; i< corretAnswer.length; i++) {
+                correctArray.push(corretAnswer[i].value)
+            }   
+
+            for (var i = 0; i< correctArray.length;i++){
+                var msg = ""
+                    if (guestAnswer[i] == correctArray[i]) {
+                        msg =  "Correct     " + correctArray[i]     
+                    }
+                    else {
+                        msg = "Wrong     " + correctArray[i]
+                }
+                $(".final").html(msg)
+            }
     })
 })
